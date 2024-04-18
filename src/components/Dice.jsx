9cd2fad7ce6deb29bcb034bef30710a613bd90af
Gizem <109289446+gizemayegul@users.noreply.github.com>
2024-04-18 17:16:00 +0200

    solution added

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
-import blank from '../assets/images/dice-empty.png';
-import one from '../assets/images/dice1.png';
-import two from '../assets/images/dice2.png';
-import three from '../assets/images/dice3.png';
-import four from '../assets/images/dice4.png';
-import five from '../assets/images/dice5.png';
-import six from '../assets/images/dice6.png';
+import { useState } from "react";
+import blank from "../assets/images/dice-empty.png";
+import one from "../assets/images/dice1.png";
+import two from "../assets/images/dice2.png";
+import three from "../assets/images/dice3.png";
+import four from "../assets/images/dice4.png";
+import five from "../assets/images/dice5.png";
+import six from "../assets/images/dice6.png";
 
 function Dice() {
   const [currentDice, setCurrentDice] = useState(blank);
@@ -22,7 +22,7 @@ function Dice() {
     <div>
       <img
         onClick={rollDice}
-        style={{ height: '100px' }}
+        style={{ height: "100px" }}
         src={currentDice}
         alt="dice"
       />
